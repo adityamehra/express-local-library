38d3e68f0ce7e6df76f59ea2966592e151f1f07d
Aditya Mehra <mehraaditya71@gmail.com>
2017-08-02 21:35:29 -0700

    added route for books' catalog

--- routes/catalog_routes.js
+++ routes/catalog_routes.js
@@ -5,6 +5,7 @@ var Author = require('../models/author')
 var authors_controller = require('../controllers/authors_controller');
 var books_controller = require('../controllers/books_controller');
 var genre_controller = require('../controllers/genre_controller');
+var book_instance_controller = require('../controllers/book_instance_controller')
 
 /* GET routes for author listings. */
 router.get('/authors', authors_controller.getAllAuthors);
@@ -18,5 +19,7 @@ router.get('/book/:id', books_controller.getBook);
 router.get('/genres', genre_controller.getAllGenres);
 router.get('/genres/:id', genre_controller.getGenre);
 // router.get('/genres/info/:id', genre_controller.getAllGenreInfo);
+
+router.get('/bookinstances', book_instance_controller.getAllInstances);
 
 module.exports = router;
